126c1714fe848d727de804aa8cfad8b6431f72da
Ronaldo Borges <ronaldo.borges@venko.com.br>
2026-03-16 09:14:09 -0300

    add main and Cmake

---
+++ main.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <unistd.h>
+
+void* tarefa_thread(void* arg) {
+    char* nome = (char*)arg;
+    for (int i = 0; i < 3; i++) {
+        printf("[Thread %s] Executando passo %d...\n", nome, i + 1);
+        sleep(1);
+    }
+    return NULL;
+}
+
+int main() {
+
+    pthread_t t1, t2;
+
+
+    pthread_create(&t1, NULL, tarefa_thread, "A");
+    pthread_create(&t2, NULL, tarefa_thread, "B");
+
+    pthread_join(t1, NULL);
+    pthread_join(t2, NULL);
+
+    printf("Projeto finalizado com sucesso.\n");
+    return 0;
+}
